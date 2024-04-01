ab9a459b7f7889a8c7593d0398a3a727db08a538
muhammadabrar02 <abrarsolangi2002@gmail.com>
2024-04-01 23:56:05 +0500

    Added ATM Machine Program

---
+++ ATM/main.ts
@@ -0,0 +1,59 @@
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollar
+
+let myPIN = 1234;
+
+let pinAnswer = await inquirer.prompt(
+    [
+
+        {
+            name: 'pin',
+            message: "Enter your PIN",
+            type: "number",
+        }
+    ]
+);
+if (pinAnswer.pin === myPIN) {
+    console.log("Correct PIN Code ");
+
+    let OperationAns = await inquirer.prompt(
+        [
+
+            {
+                name: "Operation",
+                message: "Please select Option",
+                type: "list",
+                choices: ["Withdraw", "check balance"]
+            }
+        ]
+    );
+    if (OperationAns.Operation === 'Withdraw') {
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "Amount",
+                    message: "Enter your amount",
+                    type: "number"
+                }
+
+
+            ]
+        );
+        if (amountAns.Amount > myBalance) {
+            console.log("Insufficient balance");
+            console.log("---------Thank You----------");
+        } else {
+            myBalance -= amountAns.Amount;
+            console.log("Your remaining balance is : " + myBalance);
+
+        }
+
+    } else if (OperationAns.Operation === 'check balance') {
+
+        console.log("your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect PIN number");
+}
