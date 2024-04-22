6bed662ccc9d47c5e713e9dcc30277bd7f3fb9ab
muhammadabrar02 <abrarsolangi2002@gmail.com>
2024-04-23 02:10:45 +0500

    Added Word-Counter

---
+++ Word-Counter/index.ts
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+import inquirer from "inquirer"
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your Sentence to count the word: "
+    }
+])
+
+const words=answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word counter is ${words.length}`);
